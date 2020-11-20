730833f92a65d915724c1fcc2b48c9f76764bc56
kkjumin <kkjumin@gmail.com>
2020-11-20 16:03:05 +0900

    슬라이드 추가

--- src/components/slideMenu.vue
+++ src/components/slideMenu.vue
@@ -1,19 +1,32 @@
 <template>
-  <v-carousel
-    cycle
-    height="400"
-    hide-delimiter-background
-    show-arrows-on-hover
-    style="max-width:1024px; margin:auto"
-  >
-    <v-carousel-item v-for="(slide, i) in slides" :key="i">
-      <v-sheet :color="colors[i]" height="100%">
-        <v-row class="fill-height" align="center" justify="center">
-          <div class="display-3">{{ slide }} Slide</div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
+  <div class="content-view">
+    <!-- img slide -->
+    <v-carousel
+      height="500"
+      cycle
+      style="max-width:1200px; margin:auto; margin-bottom:40px;"
+    >
+      <v-carousel-item v-for="(item, i) in items" :key="i">
+        <v-parallax :src="item.src"> </v-parallax>
+      </v-carousel-item>
+    </v-carousel>
+
+    <v-carousel
+      cycle
+      height="500"
+      hide-delimiter-background
+      show-arrows-on-hover
+      style="max-width:1200px; margin:auto; margin-bottom:40px;"
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="display-3">{{ slide }} Slide</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
@@ -28,6 +41,20 @@ export default {
         'deep-purple accent-4',
       ],
       slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/parallax/material2.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+      ],
     };
   },
 };
