5f682703ad6bf15d472e475913b3ae8f036743c3
kkjumin <kkjumin@gmail.com>
2020-11-26 17:25:49 +0900

    브라우저 체크 알림

---
+++ src/components/Alert.vue
@@ -0,0 +1,63 @@
+<template>
+  <div v-if="show" class="main-alert">
+    <v-alert prominent type="error">
+      <v-row align="center">
+        <v-col class="grow">
+          본 사이트는
+          <img
+            class="chrome-icon"
+            src="../assets/img/chrome.png"
+            alt="chrome"
+            @click="link()"
+          />Chrome기반의 브라우저에 최적화되어 있습니다.
+        </v-col>
+
+        <v-btn class="close-btn" @click="show = false">CLOSE</v-btn>
+      </v-row>
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: true,
+  }),
+  computed: {
+    isChrome() {
+      const browser = navigator.userAgent.toLowerCase();
+
+      return browser.includes('chrome') ? true : false;
+    },
+  },
+  methods: {
+    link() {
+      window.open('https://www.google.com/intl/ko/chrome/');
+    },
+  },
+};
+</script>
+
+<style>
+.main-alert {
+  position: fixed;
+  bottom: 10px;
+  left: 0;
+  width: calc(100% - 20px);
+  margin: 10px 10px;
+  z-index: 100;
+}
+
+.close-btn {
+  margin-right: 40px;
+}
+
+.chrome-icon {
+  width: 20px;
+  height: 20px;
+  margin-top: 5px;
+  vertical-align: middle;
+  margin-bottom: 8px;
+  cursor: pointer;
+}
+</style>
