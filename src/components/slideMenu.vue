caddf68b50fb50e26da7b4dfb839dce91aac1821
kkjumin <kkjumin@gmail.com>
2020-12-03 17:31:16 +0900

    .

--- src/components/slideMenu.vue
+++ src/components/slideMenu.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="content-view">
     <!-- img slide -->
-    <v-carousel
-      height="500"
-      cycle
-      style="max-width:1200px; margin:auto; margin-bottom:40px;border-radius:50px"
-    >
+    <v-carousel height="500" cycle class="slide-banner">
       <v-carousel-item v-for="(item, i) in items" :key="i">
         <v-parallax :src="item.src"> </v-parallax>
       </v-carousel-item>
     </v-carousel>
-
-    <!-- <v-carousel
-      cycle
-      height="500"
-      hide-delimiter-background
-      show-arrows-on-hover
-      style="max-width:1200px; margin:auto; margin-bottom:40px;"
-    >
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="colors[i]" height="100%">
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="display-3">{{ slide }} Slide</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel> -->
   </div>
 </template>
 
@@ -33,14 +13,6 @@
 export default {
   data() {
     return {
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4',
-      ],
-      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
       items: [
         {
           src: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
@@ -59,3 +31,12 @@ export default {
   },
 };
 </script>
+
+<style>
+.slide-banner {
+  max-width: 1200px;
+  margin: auto;
+  margin-bottom: 40px;
+  border-radius: 50px;
+}
+</style>
